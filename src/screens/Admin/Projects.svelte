7130af2aa47b442d2433db2023a1c46e57bd7173
vmkevv <vargaskevv@gmail.com>
2020-06-20 20:24:34 -0400

    project struct and file

---
+++ src/screens/Admin/Projects.svelte
@@ -0,0 +1,30 @@
+<script>
+  import { createEventDispatcher } from 'svelte'
+  import Icon from 'svelte-awesome'
+  import { times } from 'svelte-awesome/icons'
+  export let projects = [{
+    id: 1,
+    created_at: "2020-06-19T15:45:25.67903Z",
+    updated_at: "2020-06-19T15:45:25.67903Z",
+    deleted_at: "0001-01-01T00:00:00Z",
+    name: "project 1",
+    institute: "Some institute",
+  }]
+  const dispatch = createEventDispatcher()
+</script>
+
+{#each projects as project}
+  <article>
+    <div>
+      <h3>{project.id}</h3>
+      <span>
+        <Icon data="{times}" scale="1.5" class="modalIcon" />
+      </span>
+    </div>
+    <hr>
+    <p>
+      Creado el: {project.created_at}
+    </p>
+  </article>
+{/each}
+
